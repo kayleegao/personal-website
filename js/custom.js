4c76b2115a160e4598812f33e410d2bfee115c09
kayleegao <yifei.gao@mail.utoronto.ca>
2022-06-06 22:26:20 +0800

    updated contact and svg logo

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,24 @@ $(document).ready(function() {
   $("#footer-placeholder").load("../footer.html");
 });
 
+window.onload = function () {
+  lax.init()
+
+  // Add a driver that we use to control our animations
+  lax.addDriver('scrollY', function () {
+    return window.scrollY
+  })
+
+  // Add animation bindings to elements
+  lax.addElements('.selector', {
+    scrollY: {
+      translateX: [
+        ["elInY", "elCenterY", "elOutY"],
+        [0, 'screenWidth/2', 'screenWidth'],
+      ]
+    }
+  })
+}
 
 $("#nav-button").click(function(){
   $(this).toggleClass("open");
